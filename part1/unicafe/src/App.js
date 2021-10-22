98004b537f5b95ea4d979092dff2596a823864ba
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-10-22 21:18:00 +0700

    complete 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,10 +5,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const setGoodValue = () => {
+    setGood(good + 1);
+  }
+
+  const setNeutralValue = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const setBadValue = () => {
+    setBad(bad + 1);
+  }
+
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <h1>Give Feedback</h1>
+      <button onClick={setGoodValue}>Good</button>
+      <button onClick={setNeutralValue}>Neutral</button>
+      <button onClick={setBadValue}>Bad</button>
+
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </>
   )
 }
 
